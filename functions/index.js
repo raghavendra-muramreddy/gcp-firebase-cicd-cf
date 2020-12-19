263eb863df248a6508333e4e91694861e67d67ba
raghavendra-muramreddy <raghava0460@gmail.com>
2020-12-20 01:23:24 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -19,11 +19,9 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!", { structuredData: true });
     firestore.collection("demo").doc("doc").set({"name":"Raghava"}).then(docref => {
         console.log("**********document uploaded*********");
+        response.send("success!");
         return "success";
-    }).catch(function(error) {
-        console.error("Error adding document: ", error);
-        return "error";
-      });
+    });
     response.send("Hello from Firebase!");
 });
  
